30ee9e5f354db185e6ecf7aa0046f80bd4be215c
Matias Skjetne <matias.skjetne@nhn.no>
2022-11-10 13:48:53 +0100

    add comments

--- nvim/lua/config/vim-go.lua
+++ nvim/lua/config/vim-go.lua
@@ -3,6 +3,7 @@ local keymap = vim.api.nvim_set_keymap
 local default_opts = {noremap = true, silent = true}
 
 function M.setup()
+    -- keybinds for godebug
     keymap("n", "<leader>S", "<Cmd>GoDebugStart<CR>", default_opts)
     keymap("n", "<leader>n", "<Cmd>GoDebugNext<CR>", default_opts)
     keymap("n", "<leader>s", "<Cmd>GoDebugStep<CR>", default_opts)
@@ -10,6 +11,8 @@ function M.setup()
     keymap("n", "<leader>c", "<Cmd>GoDebugContinue<CR>", default_opts)
     keymap("n", "<leader>b", "<Cmd>GoDebugBreakpoint<CR>", default_opts)
 
+    -- prevent godebug from spewing hundreds of lines of debug messages into the
+    -- debug output console
     vim.g.go_debug_log_output = ''
 end
 
